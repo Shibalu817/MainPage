98c396d731fe897b5c41e94ea48fc44a17831d9b
Shibalu817 <95207030+Shibalu817@users.noreply.github.com>
2024-04-23 14:30:42 +0800

    add JS

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function updateStatus() {
+        fetch('/http://172.20.10.9/hello')
+            .then(response => response.json())
+            .then(data => {
+                const statusText = document.getElementById('statusText');
+                const statusIndicator = document.getElementById('statusIndicator');
+                const startButton = document.getElementById('startButton');
+                const stopButton = document.getElementById('stopButton');
+
+                statusText.textContent = data.status;
+                if (data.status === '運轉中') {
+                    statusIndicator.className = 'active';
+                    startButton.style.display = 'none';
+                    stopButton.style.display = 'block';
+                } else if (data.status === '待命') {
+                    statusIndicator.className = 'standby';
+                    startButton.style.display = 'block';
+                    stopButton.style.display = 'none';
+                } else {
+                    statusIndicator.style.display = 'none';
+                    startButton.style.display = 'none';
+                    stopButton.style.display = 'none';
+                }
+            });
+    }
+
+    setInterval(updateStatus, 5000);  // 每 5 秒更新狀態
+});
